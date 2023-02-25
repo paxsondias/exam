4020bcd392a70a4076377248baa0e7fbe9a6f96d
admin <admin@Bcalab4-m25>
2023-02-25 10:38:43 +0530

    First commit

--- exam1/src/exam1/SnackJar.java
+++ exam1/src/exam1/SnackJar.java
@@ -1,11 +1,12 @@
 package exam1;
 
 public class SnackJar {
-	int num_biscuits;
+	int num_biscuits,Stringjar_color;
 	SnackJar(int num_biscuits)
 	{
 		this.num_biscuits= num_biscuits;
 	}
+	Stringjar_color;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
